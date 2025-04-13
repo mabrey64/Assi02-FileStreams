15052db82cd2facf1ba7baebd91386bce03361b1
mabreydn <mabreydn@mail.uc.edu>
2025-04-13 14:38:38 -0400

    Start of Assignment
    Created the Assignment 2 project folder and copied over the Product code, making edits to the code for the new project requirements.

---
+++ src/Product.java
@@ -0,0 +1,41 @@
+/**
+
+ */
+
+public class Product
+{
+    private String ID;
+    private String Name;
+    private String Description;
+    private double Cost;
+
+    /**
+     * The constructor for the Product class
+     * @param ID The ID of the product
+     * @param Name The name of the product
+     * @param Description The description of the product
+     * @param Cost The cost of the product
+     */
+    public Product(String ID, String Name, String Description, double Cost)
+    {
+        this.ID = ID;
+        this.Name = Name;
+        this.Description = Description;
+        this.Cost = Cost;
+    }
+
+    public String formatField(String ID, String Name, String Description) {
+        return String.format("%-6s %-75s %-35s", ID, Name, Description);
+    }
+
+    /**
+     * The toString method is used to return the value of the attributes as a string
+     * @return The value of the attributes as a string
+     */
+//    @Override
+    public String toString() {
+        return ID + ", " + Name + ", " + Description + ", " + Cost;
+    }
+
+
+}
